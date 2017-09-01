a3cc57c95ccbd17d75ee0380610ba051edc2e5de
Diego Centelles Beltran <centelld@uji.es>
2017-09-01 08:38:07 +0200

    fix identation

--- src/Loggable.cpp
+++ src/Loggable.cpp
@@ -12,143 +12,143 @@
 
 namespace cpplogging {
 
-namespace spd = spdlog;
+  namespace spd = spdlog;
 
-Loggable::Loggable(std::string logname)
-{
-        Level = LogLevel::debug;
+  Loggable::Loggable(std::string logname)
+  {
+    Level = LogLevel::debug;
     auto stdout_sink = spdlog::sinks::stdout_sink_mt::instance();
     console_sink = std::make_shared<spdlog::sinks::ansicolor_sink>(stdout_sink);
     dist_sink = std::make_shared<spdlog::sinks::dist_sink_mt>();
     dist_sink->add_sink(console_sink);
     logToConsole = true;
 
-        SetLogName(logname);
+    SetLogName(logname);
 
 
 
-}
+  }
 
-Loggable::~Loggable() {
-        spd::drop(LogName);
-}
+  Loggable::~Loggable() {
+    spd::drop(LogName);
+  }
 
-void Loggable::LogToConsole(bool _logtoconsole)
-{
+  void Loggable::LogToConsole(bool _logtoconsole)
+  {
     if(logToConsole)
     {
-        if (!_logtoconsole)
-        {
-            dist_sink->remove_sink(console_sink);
-            logToConsole = false; // == _logtoconsole
-        }
+      if (!_logtoconsole)
+      {
+        dist_sink->remove_sink(console_sink);
+        logToConsole = false; // == _logtoconsole
+      }
     }
     else
     {
-        if(_logtoconsole)
-        {
-            dist_sink->add_sink(console_sink);
-            logToConsole = true;
-        }
+      if(_logtoconsole)
+      {
+        dist_sink->add_sink(console_sink);
+        logToConsole = true;
+      }
     }
-}
+  }
 
-void Loggable::LogToFile(const std::string & filename )
-{
+  void Loggable::LogToFile(const std::string & filename )
+  {
     auto fileSink = std::make_shared<spd::sinks::simple_file_sink_mt>
-            (filename);
+        (filename);
     dist_sink->add_sink(fileSink);
-}
+  }
 
-void Loggable::FlushLog()
-{
+  void Loggable::FlushLog()
+  {
     Log->flush();
-}
+  }
 
-spdlog::level::level_enum Loggable::GetSpdLevel(LogLevel _level)
-{
+  spdlog::level::level_enum Loggable::GetSpdLevel(LogLevel _level)
+  {
     switch(_level)
     {
-    case critical:
+      case critical:
         return spd::level::critical;
         break;
-    case debug:
+      case debug:
         return spd::level::debug;
         break;
-    case err:
+      case err:
         return spd::level::err;
         break;
-    case info:
+      case info:
         return spd::level::info;
         break;
-    case off:
+      case off:
         return spd::level::off;
         break;
-    case trace:
+      case trace:
         return spd::level::trace;
         break;
-    case warn:
+      case warn:
         return spd::level::warn;
         break;
     }
     return spd::level::info;
-}
+  }
 
-void Loggable::FlushLogOn(LogLevel level)
-{
+  void Loggable::FlushLogOn(LogLevel level)
+  {
     Log->flush_on(GetSpdLevel(level));
-}
+  }
 
-void Loggable::SetLogName(std::string newname)
-{
-	if(newname != LogName)
-	{
-		if(Log)
-		{
-			spd::drop(LogName);
-		}
-		LogName = newname;
+  void Loggable::SetLogName(std::string newname)
+  {
+    if(newname != LogName)
+    {
+      if(Log)
+      {
+        spd::drop(LogName);
+      }
+      LogName = newname;
 
-		Log = spd::get(LogName);
-		if(!Log)
-		{
-	    Log= std::make_shared<spdlog::logger>(LogName, dist_sink);
-		}
-		SetLogLevel(Level);
-	}
-}
+      Log = spd::get(LogName);
+      if(!Log)
+      {
+        Log= std::make_shared<spdlog::logger>(LogName, dist_sink);
+      }
+      SetLogLevel(Level);
+    }
+  }
 
-void Loggable::SetLogLevel(LogLevel _level)
-{
-	Level = _level;
-	if(Log)
-	{
-		switch(_level)
-		{
-		case critical:
-			Log->set_level(spd::level::critical);
-			break;
-		case debug:
-			Log->set_level(spd::level::debug);
-			break;
-		case err:
-			Log->set_level(spd::level::err);
-			break;
-		case info:
-			Log->set_level(spd::level::info);
-			break;
-		case off:
-			Log->set_level(spd::level::off);
-			break;
-		case trace:
-			Log->set_level(spd::level::trace);
-			break;
-		case warn:
-			Log->set_level(spd::level::warn);
-			break;
-		}
-	}
-}
+  void Loggable::SetLogLevel(LogLevel _level)
+  {
+    Level = _level;
+    if(Log)
+    {
+      switch(_level)
+      {
+        case critical:
+          Log->set_level(spd::level::critical);
+          break;
+        case debug:
+          Log->set_level(spd::level::debug);
+          break;
+        case err:
+          Log->set_level(spd::level::err);
+          break;
+        case info:
+          Log->set_level(spd::level::info);
+          break;
+        case off:
+          Log->set_level(spd::level::off);
+          break;
+        case trace:
+          Log->set_level(spd::level::trace);
+          break;
+        case warn:
+          Log->set_level(spd::level::warn);
+          break;
+      }
+    }
+  }
 
 
 
